71abcb678edb5159a73ab6487b6621d494d78733
(no author) <(no author)@c253d967-d769-4597-b660-ecf805b65d5f>
2014-06-27 19:01:03 +0000

    anon-access = write
    git-svn-id: svn://192.168.7.3/decaduino@2 c253d967-d769-4597-b660-ecf805b65d5f

---
+++ DecaDuino.cpp
@@ -0,0 +1,8 @@
+// DecaDuino.cpp
+//
+// DecaWave DW1000 driver for Arduino
+// See the README file in this directory fdor documentation
+
+#include "DecaDuino.h"
+
+
---
+++ DecaDuino.h
@@ -0,0 +1,31 @@
+// DecaDuino.h
+//
+// DecaWave DW1000 driver for Arduino
+// See the README file in this directory fdor documentation
+// 
+/// \mainpage DecaDuino library for Arduino
+///
+/// This is the main page
+///
+/// \par Supported Hardware
+///
+/// \par Installation
+/// To install, unzip the library into the libraries sub-directory of your
+/// Arduino application directory. Then launch the Arduino environment; you
+/// should see the library in the Sketch->Import Library menu, and example
+/// code in
+///
+/// \par Revision History
+/// \version 1.0 Original release
+/// 
+/// To use the DecaDuino library, you must have
+/// \code
+/// #include <DecaDuino.h>
+/// \endcode
+/// At the top of your sketch.
+/// 
+
+#ifndef DecaDuino_h
+#define DecaDuino_h
+
+#endif
