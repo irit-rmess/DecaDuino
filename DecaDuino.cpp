16e8c5f1b21c130a9c83166f014b26b26a0b70df
(no author) <(no author)@c253d967-d769-4597-b660-ecf805b65d5f>
2014-08-03 08:51:02 +0000

    commit aveugle sur macbook
    git-svn-id: svn://192.168.7.3/decaduino@3 c253d967-d769-4597-b660-ecf805b65d5f

--- DecaDuino.cpp
+++ DecaDuino.cpp
@@ -1,8 +1,213 @@
 // DecaDuino.cpp
 //
 // DecaWave DW1000 driver for Arduino
-// See the README file in this directory fdor documentation
+// See the README file in this directory for documentation
 
 #include "DecaDuino.h"
 
+DecaDuino* DecaDuino::_DecaDuinoInterrupt[MAX_NB_DW1000_FOR_INTERRUPTS] = {0, 0, 0};
+
+DecaDuino::DecaDuino(uint8_t slaveSelectPin, uint8_t interruptPin) {
+
+  _slaveSelectPin = slaveSelectPin;
+  _interruptPin = interruptPin;
+}
+
+
+boolean DecaDuino::init() {
+
+  uint16_t i;
+  uint32_t u32tmp;
+
+  // Wait for DW1000 POR (up to 5msec)
+  delay(5);
+
+  // Initialise the IRQ and Slave Select pin
+  pinMode(_interruptPin, INPUT);
+  pinMode(_slaveSelectPin, OUTPUT);
+  digitalWrite(_slaveSelectPin, HIGH);
+
+  // Initialise the SPI port
+  spi4teensy3::init(1,0,0);
+  CORE_PIN13_CONFIG = PORT_PCR_MUX(1); // First reassign pin 13 to Alt1 so that it is not SCK but the LED still works
+  CORE_PIN14_CONFIG = PORT_PCR_DSE | PORT_PCR_MUX(2); // and then reassign pin 14 to SCK
+
+  // Reset the DW1000 now
+  //resetDW1000();
+  delay(20);
+
+  // Check the device type
+  readSpi(DW1000_REGISTER_DEV_ID, buf, 4);
+  if ( decodeUint32(buf) != 0xdeca0130 ) return false;
+
+  // Attach interrupt handler
+  if (_interruptPin == 0) {
+    _DecaDuinoInterrupt[0] = this;
+    attachInterrupt(_interruptPin, DecaDuino::isr0, HIGH);
+  } else if (_interruptPin == 1) {
+    _DecaDuinoInterrupt[1] = this;
+    attachInterrupt(_interruptPin, DecaDuino::isr1, HIGH);
+  } else if (_interruptPin == 2) {
+    _DecaDuinoInterrupt[2] = this;
+    attachInterrupt(_interruptPin, DecaDuino::isr2, HIGH);
+  } else return false;
+
+  // Initialise buffer
+  for (i=0; i<BUFFER_MAX_LEN; i++)
+    buf[i] = 0;
+
+  // Configure DW1000
+  // System Event Mask Register
+  //readSpiSubAddress(0x0E, 1, buf, 1);
+  //buf[0] |= 0x20; // MRXPRD
+  //buf[0] |= 0x02; // MRXSFDD
+  //writeSpiSubAddress(0x0E, 1, buf, 1);
+
+  /*
+  readSpi(0x0E, buf, 4);
+  for (i=0; i<4; i++) {
+    Serial.print ("|");
+    Serial.print (buf[i], HEX);
+  }
+  Serial.println("|");
+  */
+
+  /*
+  setPanId(0xCAFE);
+  setShortAddress(0x1234);
+  Serial.print("PANid: ");
+  Serial.println (getPanId(), HEX);
+  Serial.print("Short address: ");
+  Serial.println (getShortAddress(), HEX);
+  */
+
+  // Return true if everything OK
+  return true;
+}
+
+
+void DecaDuino::resetDW1000() {
+
+  uint32_t u32tmp;
+
+  readSpi(DW1000_REGISTER_PMSC_CTRL0, buf, 4);
+  u32tmp = decodeUint32(buf);
+  encodeUint32( ( u32tmp & 0xFFFFFFFFC ) | 1, buf);
+  writeSpi(DW1000_REGISTER_PMSC_CTRL0, buf, 4);
+  encodeUint32( u32tmp & 0x0FFFFFFFF, buf );
+  writeSpi(DW1000_REGISTER_PMSC_CTRL0, buf, 4);
+  encodeUint32( u32tmp | 0xF0000000, buf );
+  writeSpi(DW1000_REGISTER_PMSC_CTRL0, buf, 4);
+}
+
+
+void DecaDuino::isr0() {
+
+  Serial.println("isr0");
+  if (_DecaDuinoInterrupt[0]) _DecaDuinoInterrupt[0]->handleInterrupt();
+}
+
+
+void DecaDuino::isr1() {
+
+  Serial.println("isr1");
+  if (_DecaDuinoInterrupt[1]) _DecaDuinoInterrupt[1]->handleInterrupt();
+}
+
+
+void DecaDuino::isr2() {
+
+  Serial.println("isr2");
+  if (_DecaDuinoInterrupt[2]) _DecaDuinoInterrupt[2]->handleInterrupt();
+}
+
+void DecaDuino::handleInterrupt() {
+
+}
+
+void DecaDuino::_my_handleInterrupt() {
+
+  uint32_t statusReg;
+  uint16_t frameLen;
+  uint16_t i;
+
+  readSpi(DW1000_REGISTER_SYS_STATUS, buf, 4);
+  statusReg = decodeUint32(buf);
+
+  Serial.print("interrupt mask: ");
+  sprintf((char*)buf,"%x\n", statusReg);
+  Serial.println((char*)buf);
+
+  if ( statusReg & DW1000_REGISTER_SYS_STATUS_RXDFR_MASK ) { // RXDFR
+
+    //if ( statusReg & DW1000_REGISTER_SYS_STATUS_RXFCG_MASK ) { // RXFCG
+
+???MANY LINES MISSING
+void DecaDuino::readSpiSubAddress(uint8_t address, uint8_t subAddress, uint8_t* buf, uint8_t len) {
+
+  uint8_t addr = 0 | (address & 0x3F) | 0x40; // Mask register address (6bits) and preserve MSb at low (Read) and subaddress (0x40)
+  uint8_t sub_addr = 0 | (subAddress & 0x3F); // Mask register address (6bits)
+
+  digitalWrite(_slaveSelectPin, LOW);
+  spi4teensy3::send(addr);
+  spi4teensy3::send(sub_addr);
+  spi4teensy3::receive(buf,len);
+  digitalWrite(_slaveSelectPin, HIGH);
+}
+
+
+uint16_t DecaDuino::decodeUint16 ( uint8_t *data ) {
+
+  return 0 | (data[1] << 8) | data[0];
+}
+
+
+void DecaDuino::encodeUint16 ( uint16_t from, uint8_t *to ) {
+
+  to[1] = (from & 0xFF00) >> 8;
+  to[0] = from & 0xFF;
+}
+
+
+uint32_t DecaDuino::decodeUint32 ( uint8_t *data ) {
+
+  return 0 | (data[3] << 24) | (data[2] << 16) | (data[1] << 8) | data[0];
+}
+
+
+void DecaDuino::encodeUint32 ( uint32_t from, uint8_t *to ) {
+
+  to[3] = (from & 0xFF000000) >> 24;
+  to[2] = (from & 0xFF0000) >> 16;
+  to[1] = (from & 0xFF00) >> 8;
+  to[0] = from & 0xFF;
+}
+
+
+uint16_t DecaDuino::getPanId() {
+
+  readSpiSubAddress(DW1000_REGISTER_PANADR, DW1000_REGISTER_PANADR_PANID_OFFSET, buf, 2);
+  return decodeUint16(buf);
+}
+
+
+void DecaDuino::setPanId(uint16_t panId) {
+
+  encodeUint16(panId, buf);
+  writeSpiSubAddress(DW1000_REGISTER_PANADR, DW1000_REGISTER_PANADR_PANID_OFFSET, buf, 2);
+}
+
+
+uint16_t DecaDuino::getShortAddress() {
+
+  readSpiSubAddress(DW1000_REGISTER_PANADR, DW1000_REGISTER_PANADR_SHORT_ADDRESS_OFFSET, buf, 2);
+  return decodeUint16(buf);
+}
+
+
+void DecaDuino::setShortAddress(uint16_t shortAddress) {
+
+  encodeUint16(shortAddress, buf);
+  writeSpiSubAddress(DW1000_REGISTER_PANADR, DW1000_REGISTER_PANADR_SHORT_ADDRESS_OFFSET, buf, 2);
+}
 
--- DecaDuino.h
+++ DecaDuino.h
@@ -28,4 +28,97 @@
 #ifndef DecaDuino_h
 #define DecaDuino_h
 
+#include "Arduino.h"
+#include <spi4teensy3.h>
+
+#define DW1000_IRQ0_PIN 0
+#define DW1000_IRQ1_PIN 1
+#define DW1000_IRQ2_PIN 2
+#define DW1000_CS0_PIN 10
+#define DW1000_CS1_PIN 10 // ToDo: check Teensy3.1 other SlaveSelect pins
+#define DW1000_CS2_PIN 10 // ToDo: check Teensy3.1 other SlaveSelect pins
+#define MAX_NB_DW1000_FOR_INTERRUPTS 3
+#define BUFFER_MAX_LEN 1024
+
+#define DW1000_REGISTER_DEV_ID 		0x00
+#define DW1000_REGISTER_EUI 		0x01
+#define DW1000_REGISTER_PANADR		0x03
+#define DW1000_REGISTER_SYS_MASK	0x0E
+#define DW1000_REGISTER_SYS_STATUS	0x0F
+#define DW1000_REGISTER_PMSC_CTRL0	0x36
+#define DW1000_REGISTER_
+
+#define DW1000_REGISTER_PANADR_SHORT_ADDRESS_OFFSET 0x00
+#define DW1000_REGISTER_PANADR_PANID_OFFSET 0x02
+
+#define DW1000_REGISTER_SYS_MASK_MRXPRD_MASK
+
+#define DW1000_REGISTER_SYS_STATUS_RXDFR_MASK 0x00002000
+#define DW1000_REGISTER_SYS_STATUS_RXFCG_MASK 0x00004000
+
+class DecaDuino {
+
+  public:
+    DecaDuino(uint8_t slaveSelectPin = DW1000_CS0_PIN, uint8_t interruptPin = DW1000_IRQ0_PIN);
+    boolean init();
+    void resetDW1000();
+    void dummy();
+    void writeSpi(uint8_t address, uint8_t* buf, uint8_t len);
+    void readSpi(uint8_t address, uint8_t* buf, uint8_t len);
+    void writeSpiSubAddress(uint8_t address, uint8_t subAddress, uint8_t* buf, uint8_t len);
+    void readSpiSubAddress(uint8_t address, uint8_t subAddress, uint8_t* buf, uint8_t len);
+    uint16_t getPanId();
+    void setPanId(uint16_t panId);
+    uint16_t getShortAddress();
+    void setShortAddress(uint16_t shortAddress);
+    void plmeRxEnableRequest(void);
+    void plmeDataRequest(uint8_t* buf, uint8_t len);
+
+    void _my_handleInterrupt(); //temp
+
+  private:
+    /*
+    * Return a UINT16 based on two UINT8
+    * @author Adrien van den Bossche <bossche@irit.fr>
+    * @date 20111123
+    */
+    uint16_t decodeUint16 ( uint8_t *data );
+
+    /*
+    * Place data from at to address
+    * No return
+    * @author Adrien van den Bossche <bossche@irit.fr>
+    * @date 20111011
+    */
+    void encodeUint16 ( uint16_t from, uint8_t *to );
+
+    /*
+    * Return a UINT32 based on two UINT8
+    * @author Adrien van den Bossche <bossche@irit.fr>
+    * @date 20111123
+    */
+    uint32_t decodeUint32 ( uint8_t *data );
+
+    /*
+    * Place data from at to address
+    * No return
+    * @author Adrien van den Bossche <bossche@irit.fr>
+    * @date 20111011
+    */
+    void encodeUint32 ( uint32_t from, uint8_t *to );
+
+    uint8_t buf[BUFFER_MAX_LEN];
+
+  protected:
+    static void isr0();
+    static void isr1();
+    static void isr2();
+    uint8_t _slaveSelectPin;
+    uint8_t _interruptPin;
+
+    static DecaDuino* _DecaDuinoInterrupt[];
+    void handleInterrupt();
+};
+
+
 #endif
